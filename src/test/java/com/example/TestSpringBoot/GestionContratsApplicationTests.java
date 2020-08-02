df9c6251d064f5df58a3273bc705e077fe2b24ec
Abounki Hamza <abounkihamza@gmail.com>
2020-08-02 16:24:50 +0100

    first commit

---
+++ src/test/java/com/example/TestSpringBoot/GestionContratsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionContratsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
